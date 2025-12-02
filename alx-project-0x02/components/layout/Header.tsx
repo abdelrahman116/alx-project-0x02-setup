cfb740a4c41db18860c6c7de922d33d52b2545f8
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-12-02 19:17:36 +0200

    Homw&About

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <div>
       <h1>This is my header</h1>
+      <ul>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/home">
+          <li>Home</li>
+        </Link>
+      </ul>
     </div>
   );
 }
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,6 +1,8 @@
+import Header from "@/components/layout/Header";
 export default function Home() {
   return (
     <main>
+      <Header />
       <h1 style={{ fontSize: 48 }}>Hello from alx-project-0x02 👋</h1>
     </main>
   );
