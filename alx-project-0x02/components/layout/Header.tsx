e13fc9b1249c86646e80652b2829a2bee19f1f7e
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-12-02 18:55:56 +0200

    Fisrst task

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>This is my header</h1>
+    </div>
+  );
+}
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 style={{ fontSize: 48 }}>Hello from alx-proje;klm'ct-0x02 👋</h1>
+      <h1 style={{ fontSize: 48 }}>Hello from alx-project-0x02 👋</h1>
     </main>
   );
 }
